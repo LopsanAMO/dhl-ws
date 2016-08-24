fafc59fe01467f73d5a5c56e64837723ee63cd25
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-08-24 23:37:46 +0300

    Actualizo la clase centralizadora DHL

--- dhl/__init__.py
+++ dhl/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 import ws
 
 
@@ -9,19 +7,10 @@ class DHL:
     """
     services = ['GetQuote', 'GetCapability']
 
-    def __init__(self):
-        self.site_id = os.getenv('DHL_SITE_ID')
-        self.password = os.getenv('DHL_PASSWORD')
-        self.account_number = os.getenv('DHL_ACCOUNT_NUMBER')
-
     def service(self, service_name, **kwargs):
         if service_name in self.services:
             class_name = 'DHL' + service_name
-            service = getattr(ws, class_name)(self.site_id,
-                                              self.password,
-                                              self.account_number,
-                                              service_name,
-                                              **kwargs)
+            service = getattr(ws, class_name)(service_name, **kwargs)
             return service.request()
         raise DHLError("The service {} does not exist".format(service_name))
 

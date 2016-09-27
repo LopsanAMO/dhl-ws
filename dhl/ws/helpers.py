3038d92f9acf2f7b6f42c939a855c1fc311e39a8
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-09-27 17:46:06 +0300

    tracking helper created

--- dhl/ws/helpers.py
+++ dhl/ws/helpers.py
@@ -90,3 +90,13 @@ def get_quote(site_id, password, account_number, from_zipcode, to_zipcode, items
     get_quote.append(build_from_to('To', to_zipcode))
     get_quote.append(build_dutiable())
     return '%s%s' % (xml, etree.tostring(root).decode('utf-8'))
+
+
+def tracking(site_id, password, awbnumber):
+    xml = '<?xml version="1.0" encoding="UTF-8"?>'
+    root = Element('req:KnownTrackingRequest', {'xmlns:req': 'http://www.dhl.com'})  # NOQA
+    root.append(build_request(site_id, password))
+    root.append(element('LanguageCode', 'en'))
+    root.append(element('AWBNumber', awbnumber))
+    root.append(element('LevelOfDetails', 'ALL_CHECK_POINTS'))
+    return '%s%s' % (xml, etree.tostring(root).decode('utf-8'))

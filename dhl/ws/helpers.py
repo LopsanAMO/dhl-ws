504ce940af4719ea161eef15cb975138ec74ccd5
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-09-29 02:17:48 +0300

    fix shipping_guide

--- dhl/ws/helpers.py
+++ dhl/ws/helpers.py
@@ -208,4 +208,5 @@ def shipping_guide(data):
     root.append(shipper(data['account_number'], data['shipper']))
     root.append(element('EProcShip', 'N'))
     root.append(element('LabelImageFormat', 'PDF'))
-    return '%s%s' % (xml, etree.tostring(root).decode('utf-8'))
+    prev = '%s%s' % (xml, etree.tostring(root).decode('utf-8'))
+    return prev.replace('ship-val-global-req.xsd', 'ship-val-global-req.xsd schemaVersion="4.0"')  # NOQA

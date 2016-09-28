39dd2e7f287b3b3a621117e24b85fc09a58fb8cd
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-09-28 05:40:13 +0300

    Update ShipmentValidation

--- dhl/ws/shipment_validation.py
+++ dhl/ws/shipment_validation.py
@@ -7,15 +7,15 @@ class DHLShipmentValidation(WSCommon):
     def __init__(self, service_name, **kwargs):
         super(DHLShipmentValidation, self).__init__(service_name, **kwargs)
 
-    def xml_request(self, **kwargs):
-        kwargs['site_id'] = self.site_id
-        kwargs['password'] = self.password
-        kwargs['account_number'] = self.account_number
+    def xml_request(self, data):
+        data['site_id'] = self.site_id
+        data['password'] = self.password
+        data['account_number'] = self.account_number
         # Needless validate 'kwargs'
-        return shipping_guide(kwargs)
+        return shipping_guide(data)
 
     def request(self):
         response = self.requests.post(self.url,
-                                      data=self.xml_request(**self.kwargs))
+                                      data=self.xml_request(self.kwargs))
         res_dict = self.jxmlease.parse(response._content)
         return res_dict

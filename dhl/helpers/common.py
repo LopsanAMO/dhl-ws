4113e0ae9a03ca7e9eec04192f55af434b5702cf
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-10-11 01:06:38 +0300

    Fix build_request

--- dhl/helpers/common.py
+++ dhl/helpers/common.py
@@ -2,14 +2,12 @@ import xml.etree.ElementTree as etree  # NOQA
 from datetime import datetime
 from xml.etree.ElementTree import Element
 
-date = datetime.now()
-
 
 def build_request(site_id, password):
     root = Element('Request')
     service_header = Element('ServiceHeader')
     root.append(service_header)
-    service_header.append(element('MessageTime', date.strftime('%Y-%m-%dT%H:%M:%S-05:00')))  # NOQA
+    service_header.append(element('MessageTime', datetime.now().strftime('%Y-%m-%dT%H:%M:%S-05:00')))  # NOQA
     service_header.append(element('MessageReference', '1234567890123456789012345678901'))  # NOQA
     service_header.append(element('SiteID', site_id))
     service_header.append(element('Password', password))
